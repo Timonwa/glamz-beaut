6e1e204330cf26c51100710a54a83ffb829353cd
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2021-09-23 14:44:36 +0100

    burger menu now opens n one click.

--- js/script.js
+++ js/script.js
@@ -3,24 +3,14 @@
 const menu = document.querySelector(".burger-menu");
 // add event listener to toggle burger menu
 menu.addEventListener("click", function () {
-    if (document.querySelector(".open").style.display === "block") {
-        document.querySelector(".open").style.display = "none";
-        document.querySelector(".close").style.display = "block";
-        document.querySelector(".nav-bar").style.top = "var(--nav-height)";
-    }
-    else {
+    if (document.querySelector(".open").style.display === "none") {
         document.querySelector(".open").style.display = "block";
         document.querySelector(".close").style.display = "none";
         document.querySelector(".nav-bar").style.top = "-50vh";
     }
-});
-
-//* adding active state to current webpage viewed by the user
-// if current file path of webpage in view is same as the file path of the nav link that was clicked, then that nav link should have a class of .active
-const navLinks = document.querySelectorAll(".nav-link");
-for (i of navLinks) {
-    var path = window.location.href;
-    if (i.href === path) {
-        i.classList.add("active");
+    else {
+        document.querySelector(".open").style.display = "none";
+        document.querySelector(".close").style.display = "block";
+        document.querySelector(".nav-bar").style.top = "var(--nav-height)";
     }
-}
+});
