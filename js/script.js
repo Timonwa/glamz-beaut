1f3e65a211cc74780d1153715868bd604c95faac
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2021-09-26 12:46:03 +0100

    modified the burger menu.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,34 @@
+// * function to close the dropdown menu on smaller screens
+function navLinkCloseMenu() {
+    document.querySelector(".open").style.display = "block";
+    document.querySelector(".close").style.display = "none";
+    document.querySelector(".nav-links").style.top = "-50vh";
+}
+
+// * function to open the dropdown menu on smaller screens
+function navLinkOpenMenu() {
+    document.querySelector(".open").style.display = "none";
+    document.querySelector(".close").style.display = "block";
+    document.querySelector(".nav-links").style.top = "var(--nav-height)";
+}
+
+
 // * burger menu toggle
-// burger menu div
+// select burger menu
 const menu = document.querySelector(".burger-menu");
 // add event listener to toggle burger menu
 menu.addEventListener("click", function () {
     if (document.querySelector(".open").style.display === "none") {
-        document.querySelector(".open").style.display = "block";
-        document.querySelector(".close").style.display = "none";
-        document.querySelector(".nav-bar").style.top = "-50vh";
+        navLinkCloseMenu()
     }
     else {
-        document.querySelector(".open").style.display = "none";
-        document.querySelector(".close").style.display = "block";
-        document.querySelector(".nav-bar").style.top = "var(--nav-height)";
+        navLinkOpenMenu()
     }
 });
+
+// * individual nav links
+// when you click on any of the links, the dropdown menu should close
+const navLinks = document.querySelectorAll(".nav-link");
+for (let link of navLinks) {
+    link.addEventListener("click", navLinkCloseMenu);
+}
