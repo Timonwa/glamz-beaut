596c7298517f7961b3cf9ead01334b27bf8785d5
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2023-01-05 14:35:41 +0100

    looks better.

--- js/script.js
+++ js/script.js
@@ -1,34 +1,32 @@
 // * function to close the dropdown menu on smaller screens
 function navLinkCloseMenu() {
-    document.querySelector(".open").style.display = "block";
-    document.querySelector(".close").style.display = "none";
-    document.querySelector(".nav-links").style.top = "-50vh";
+  document.querySelector(".open").style.display = "block";
+  document.querySelector(".close").style.display = "none";
+  document.querySelector(".nav-links").style.top = "-50vh";
 }
 
 // * function to open the dropdown menu on smaller screens
 function navLinkOpenMenu() {
-    document.querySelector(".open").style.display = "none";
-    document.querySelector(".close").style.display = "block";
-    document.querySelector(".nav-links").style.top = "var(--nav-height)";
+  document.querySelector(".open").style.display = "none";
+  document.querySelector(".close").style.display = "block";
+  document.querySelector(".nav-links").style.top = "60px";
 }
-
 
 // * burger menu toggle
 // select burger menu
 const menu = document.querySelector(".burger-menu");
 // add event listener to toggle burger menu
 menu.addEventListener("click", function () {
-    if (document.querySelector(".open").style.display === "none") {
-        navLinkCloseMenu()
-    }
-    else {
-        navLinkOpenMenu()
-    }
+  if (document.querySelector(".open").style.display === "none") {
+    navLinkCloseMenu();
+  } else {
+    navLinkOpenMenu();
+  }
 });
 
 // * individual nav links
 // when you click on any of the links, the dropdown menu should close
 const navLinks = document.querySelectorAll(".nav-link");
 for (let link of navLinks) {
-    link.addEventListener("click", navLinkCloseMenu);
+  link.addEventListener("click", navLinkCloseMenu);
 }
